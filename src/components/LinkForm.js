006ade8d09f0ae92a3386ad59a57eacf659a7cc5
cmachado17 <machado.cristian1217@gmail.com>
2020-09-22 15:58:23 -0300

    create form, add state, handle input data

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,11 +1,63 @@
-import React from 'react'
-    const LinkForm = () => {
-        return ( 
-            <form>
+import React, { useState } from "react";
 
-                
-            </form>
-         );
-    }
-     
-    export default LinkForm;
+const LinkForm = () => {
+  const initialStateValues = {
+    url: "",
+    name: "",
+    description: "",
+  };
+
+  const [values, setValues] = useState(initialStateValues);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+  return (
+    <form className="card card-body" onSubmit={handleSubmit}>
+      <div className="form-group input-group">
+        <div className="input-group-text bg-light">
+          <i className="material-icons">insert_link</i>
+        </div>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="https://someurl.com"
+          name="url"
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="form-group input-group">
+        <div className="input-group-text bg-light">
+          <i className="material-icons">create</i>
+        </div>
+        <input
+          type="text"
+          className="form-control"
+          name="name"
+          placeholder="website name"
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <textarea
+          name="description"
+          row="3"
+          className="form-control"
+          placeholder="write a description"
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <button className="btn btn-primary btn-block">Save</button>
+    </form>
+  );
+};
+
+export default LinkForm;
