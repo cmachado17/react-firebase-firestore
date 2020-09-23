91dbe1973792e617a1423ba901d5d6c221c7176d
cmachado17 <machado.cristian1217@gmail.com>
2020-09-23 13:52:09 -0300

    Updates

---
+++ precache-manifest.6dcafd8ec7a6286be14f6217b4ab89d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc8625588a78868b64434ce22174d5a",
+    "url": "/react-firebase-firestore/index.html"
+  },
+  {
+    "revision": "824da0ce0d29ab0f486c",
+    "url": "/react-firebase-firestore/static/css/2.56d22f2b.chunk.css"
+  },
+  {
+    "revision": "68f5994e7d514005fd60",
+    "url": "/react-firebase-firestore/static/css/main.f4bc1fdf.chunk.css"
+  },
+  {
+    "revision": "824da0ce0d29ab0f486c",
+    "url": "/react-firebase-firestore/static/js/2.1106e026.chunk.js"
+  },
+  {
+    "revision": "659df5b420806b55437f4f9e57f60411",
+    "url": "/react-firebase-firestore/static/js/2.1106e026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f5994e7d514005fd60",
+    "url": "/react-firebase-firestore/static/js/main.6b470b6d.chunk.js"
+  },
+  {
+    "revision": "fa95c91931bfd41f35a7",
+    "url": "/react-firebase-firestore/static/js/runtime-main.a6564175.js"
+  }
+]);
